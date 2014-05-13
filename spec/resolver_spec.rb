60694fe6d44231439012463317c8b2f82810ceb3
Chris Carpenter <ccarpenter@camber.com>
2014-05-13 14:52:11 -0500

    Add specs for nameserver=

--- spec/resolver_spec.rb
+++ spec/resolver_spec.rb
@@ -5,25 +5,102 @@ describe Net::DNS::Resolver do
     @resolv = Net::DNS::Resolver.new
   end
 
-  it "can set packet_size as integer" do
-    @resolv.packet_size = 2048
-    @resolv.packet_size.should eq 2048
-  end
-
-  it "can set packet_size as string" do
-    @resolv.packet_size = "2048"
-    @resolv.packet_size.should eq 2048
-  end
-
-  it "can set packet_size as arbitrary class" do
-    class Blah
-      def to_i
-        2048
-      end
+  describe "packet_size=" do
+    it "can set packet_size as integer" do
+      @resolv.packet_size = 2048
+      @resolv.packet_size.should eq 2048
     end
-    x = Blah.new
-    @resolv.packet_size = x
-    @resolv.packet_size.should eq 2048
+
+    it "can set packet_size as string" do
+      @resolv.packet_size = "2048"
+      @resolv.packet_size.should eq 2048
+    end
+
+    it "can set packet_size as arbitrary class" do
+      class Blah
+        def to_i
+          2048
+        end
+      end
+      x = Blah.new
+      @resolv.packet_size = x
+      @resolv.packet_size.should eq 2048
+    end
+  end
+
+  describe "nameservers=" do
+    let(:dns_ipaddr) {IPAddr.new("8.8.8.8")}
+    let(:dns_string) {"8.8.8.8"}
+
+    it "can set nameserver as IPAddr" do
+      @resolv.nameserver = dns_ipaddr
+      @resolv.nameserver.should eq [dns_string]
+    end
+
+    it "can set nameserver as String ip" do
+      @resolv.nameserver = "8.8.8.8"
+      @resolv.nameserver.should eq [dns_string]
+    end
+
+    it "can set nameserver as String domain" do
+      @resolv.nameserver = "google-public-dns-a.google.com"
+      @resolv.nameserver.should eq [dns_string]
+    end
+
+    it "can set nameserver as Array" do
+      @resolv.nameservers = [
+        "google-public-dns-a.google.com",
+        dns_ipaddr,
+        "8.8.8.8"
+      ]
+
+      @resolv.nameserver.should eq [
+        dns_string,
+        dns_string,
+        dns_string
+      ]
+    end
+
+    it "can set nameserver as Arbitrary to_a object" do
+      class Blah
+        def to_a
+          [
+            "google-public-dns-a.google.com",
+            "8.8.8.8"
+          ]
+        end
+      end
+      @resolv.nameservers = Blah.new
+      @resolv.nameserver.should eq [
+        dns_string,
+        dns_string
+      ]
+    end
+
+    it "can set nameserver as Arbitrary map object" do
+      class Blah
+        def initialize
+          @array = [
+            "google-public-dns-a.google.com",
+            "8.8.8.8"
+          ]
+        end
+
+        def map &block
+          ret = []
+          @array.each do |item|
+            ret << block.call(item)
+          end
+          ret
+        end
+      end
+
+      @resolv.nameservers = Blah.new
+      @resolv.nameserver.should eq [
+        dns_string,
+        dns_string
+      ]
+    end
   end
 
   context "packet_size set" do
@@ -34,5 +111,17 @@ describe Net::DNS::Resolver do
     it "can query" do
       @resolv.query "google.com"
     end
+  end
+
+  context "nameservers set" do
+    before :all do
+      @resolv = Net::DNS::Resolver.new({nameservers: ["8.8.8.8", "8.8.4.4"]})
+    end
+
+    it "can query" do
+      @resolv.query "google.com"
+    end
+
+    #TODO: Add tests that ensure that the correct nameservers are used for lookup
   end
 end
